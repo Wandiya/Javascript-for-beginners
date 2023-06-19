25d73c180b29feb40514a4400dcc43a72eea6d39
Wandiya James <jameswandiya@gmail.com>
2023-06-19 12:20:38 +0100

    added notes

--- scripts/beginners.js
+++ scripts/beginners.js
@@ -606,14 +606,14 @@ const bill = function(products, tax) {
 }
 console.log(bill([10, 2, 65], 0.6))
 */
-
+// Class work
 // arrow function method
 const bill = (products, tax) => {
     let total = 0;
     for (let i = 0; i < products.length; i++) {
-        total += products[i] + products[i] * tax;
+        totalgit += products[i] + products[i] * tax;
     }
     return total;
 };
 
-console.log(bill([10, 2, 65], 0.2)); // Output: 92.4
+console.log(bill([10, 12], 0.2)); //10 * 0.2 + 10 // Output: 92.4
